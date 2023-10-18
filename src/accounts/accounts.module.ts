04b9d8ff0c03d1dc2db11ab0470590e441e51c77
qwerkilll <alban.munoz40@gmail.com>
2023-10-18 13:38:20 +0200

    fix bug

--- src/accounts/accounts.module.ts
+++ src/accounts/accounts.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AccountsController } from './controller/accounts.controller';
 import { AccountsService } from './service/accounts.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AccountEntity } from './entity/accounts.entity';
 
 @Module({
+    imports: [TypeOrmModule.forFeature([AccountEntity])],
     controllers: [AccountsController],
     providers: [AccountsService],
+    exports: [AccountsService],
 })
 export class AccountsModule {}
