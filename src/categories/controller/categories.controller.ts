54ee6df3f1a749ffca260e25773d8f53156719e1
Yvann Nadal <yvann.nadal@mail-esd.com>
2023-10-18 11:23:15 +0200

    nothing

--- src/categories/controller/categories.controller.ts
+++ src/categories/controller/categories.controller.ts
@@ -1,31 +1,23 @@
-import {
-    Controller,
-    Get,
-    Param,
-    ParseIntPipe,
-    Post,
-    Body,
-  } from '@nestjs/common';
-import { CategoriesService } from '../service/categories.service';
-import { CategoriesEntity } from '../entity/categories.entity';
+import { Controller, Get, Param, ParseIntPipe, Post, Body } from "@nestjs/common";
+import { CategoriesService } from "../service/categories.service";
+import { CategoriesEntity } from "../entity/categories.entity";
 
-@Controller('categories')
-    export class CategoriesController {
-            
-            constructor(private readonly categoriesService: CategoriesService) {}
+@Controller("categories")
+export class CategoriesController {
+  constructor(private readonly categoriesService: CategoriesService) {}
 
-        @Get()
-        getAllCategories() {
-            return this.categoriesService.getAllCategories();
-        }
+  @Get()
+  getAllCategories() {
+    return this.categoriesService.getAllCategories();
+  }
 
-        @Get(':id')
-        getOneCategorieById(@Param('id', ParseIntPipe) id: number) {
-            return this.categoriesService.getOneCategorieById(id);
-            }
+  @Get(":id")
+  getOneCategorieById(@Param("id", ParseIntPipe) id: number) {
+    return this.categoriesService.getOneCategorieById(id);
+  }
 
-        @Post()
-        createCategorie(@Body() categorie: CategoriesEntity) {
-            return this.categoriesService.createCategorie(categorie);
-        }
-    }
+  @Post()
+  createCategorie(@Body() categorie: CategoriesEntity) {
+    return this.categoriesService.createCategorie(categorie);
+  }
+}
