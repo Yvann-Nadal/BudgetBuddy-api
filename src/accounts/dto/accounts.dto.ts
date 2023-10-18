0835a91c3f36ef966153fd4d913dea52081c8dfb
qwerkilll <alban.munoz40@gmail.com>
2023-10-18 12:23:44 +0200

    crud accounts

--- src/accounts/dto/accounts.dto.ts
+++ src/accounts/dto/accounts.dto.ts
@@ -1,5 +1,22 @@
-export class AccountsDto {
-    id: number;
+import { IsOptional, IsString } from "class-validator";
+
+
+
+export class AccountsCreateDTO{
+    @IsString()
     name: string;
+    @IsString()
+    balance: number;
+}
+
+export class AccountsUpdateDTO{
+    @IsOptional()
+    @IsString()
+    id: number;
+    @IsOptional()
+    @IsString()
+    name: string;
+    @IsOptional()
+    @IsString()
     balance: number;
 }
