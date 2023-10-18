278390396f00346c9fadd85fc9c7f5f4f904d065
qwerkilll <alban.munoz40@gmail.com>
2023-10-18 10:52:33 +0200

    add crud user

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -3,8 +3,14 @@ import {
     Get,
     Param,
     ParseIntPipe,
+    Post,
+    Body,
+    Put,
+    Delete,
   } from '@nestjs/common';
 import { UsersService } from '../service/user.service';
+import { UsersEntity } from '../entity/user.entity';
+import { UsersCreateDTO, UsersUpdateDTO } from '../dto/user.dto';
 
 
 
@@ -21,6 +27,24 @@ import { UsersService } from '../service/user.service';
     @Get(':id')
     getOneUserById(@Param('id', ParseIntPipe) id: number) {
       return this.usersService.getOneUserById(id);
+    }
+
+    @Post()
+    createUser(@Body() data: UsersCreateDTO) {
+      return this.usersService.createUser(data);
+    }
+
+    @Put(':id')
+    updateUser(
+      @Param('id', ParseIntPipe) id: number,
+      @Body() data: UsersUpdateDTO,
+    ) {
+      return this.usersService.updateUser(id, data);
+    }
+  
+    @Delete(':id')
+    deleteUser(@Param('id', ParseIntPipe) id: number) {
+      return this.usersService.deleteUser(id);
     }
 
     }
--- src/user/service/user.service.ts
+++ src/user/service/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { UsersEntity } from "../entity/user.entity";
 import { Repository } from "typeorm";
+import { UsersCreateDTO, UsersUpdateDTO } from "../dto/user.dto";
 
 
 
@@ -22,4 +23,26 @@ export class UsersService {
         .where('user.id = :id', { id })
         .getOne();
     }
+    
+    async createUser(data: UsersCreateDTO) {
+        try {
+          return this.userRepository.save(data);
+        } catch (error) {
+          console.log(error);
+          throw new Error('Error while creating user');
+        }
+      }
+
+      async updateUser(id: number, data: UsersUpdateDTO) {
+        const user = await this.userRepository.findOneBy({ id });
+    
+        const userUpdate = { ...user, ...data };
+        await this.userRepository.save(userUpdate);
+    
+        return userUpdate;
+      }
+
+      async deleteUser(id: number) {
+        return await this.userRepository.delete(id);
+      }
 }
